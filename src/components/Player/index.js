9f42bac4d0d52ad477cd86b11e23439e79c0931e
KristinaWolf <student.pri@yandex.ru>
2022-09-14 22:27:41 +0300

    fix controls player and remove react-youtube

--- src/components/Player/index.js
+++ src/components/Player/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactPlayer from 'react-player';
-// import YouTube from 'react-youtube';
 import './index.scss';
 
 const Player = ({idVideo, playerState, setPlayerState}) => {
   return (
     <div className="player__wrap">
-      <ReactPlayer url={`https://www.youtube.com/watch?v=${idVideo}`} />
+      <ReactPlayer controls url={`https://www.youtube.com/watch?v=${idVideo}`} />
       <div className="player__close" onClick={()=>setPlayerState(!playerState)}>✕</div>
     </div>
   );
